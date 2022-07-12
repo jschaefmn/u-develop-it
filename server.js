3a1605200f437fa6054968d78544f20109078a5f
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-11 21:58:58 -0500

    finished with 12.2.3

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Schaefb30!%',
+    database: 'election'
+  },
+  console.log('Connected to the election database')
+);
 
 app.get('/', (req, res) => {
   res.json({
