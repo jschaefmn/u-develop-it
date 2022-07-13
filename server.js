4e7e47efbc412304eb7058984d948382819f9af8
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-12 21:53:49 -0500

    finished 12.2.6

--- server.js
+++ server.js
@@ -16,43 +16,77 @@ const db = mysql.createConnection(
   console.log('Connected to the election database')
 );
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT * FROM candidates`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
   });
 });
 
-db.query(`SELECT * FROM  candidates WHERE id = 1`, (err, rows) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(rows);
+// GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
 
-// // Delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Create candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// values (?,?,?,?)`;
+
+// const params = [17, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
 //   if (err) {
 //     console.log(err);
 //   }
 //   console.log(result);
+// })
+
+// app.use((req, res) => {
+//   res.status(404).end();
 // });
-
-// Create candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-values (?,?,?,?)`;
-
-const params = [1, 'Ronald', 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
-})
-
-app.use((req, res) => {
-  res.status(404).end();
-});
 
 
 app.listen(PORT, () => {
