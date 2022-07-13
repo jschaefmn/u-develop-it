a31942ab84e6b3cb0c4dd23b5b0166f596aec1bb
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-13 09:56:52 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -72,21 +73,34 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-// values (?,?,?,?)`;
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
 
-// const params = [17, 'Ronald', 'Firbank', 1];
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
 
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// })
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
 
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 
 app.listen(PORT, () => {
